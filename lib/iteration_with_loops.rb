b7a84321d9c7e678aa75789e2ab1ba9d127acde3
Evan <evanfujita@gmail.com>
2020-12-16 21:58:05 -0600

    commit tree 81b0419205e084bb748b5b50f7caab6f1793275f

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,9 @@
+require 'pry'
+
 def find_min_in_nested_arrays(src)
-  count = 0
-  new_array = []
-  small_array = []
-  
-  while count < src.length do
-    new_array = src[count].sort
-    small_array << new_array.first
-  count += 1  
-end
-small_array
+  a = []
+  src.each do |array| 
+  a << array.min 
+  end
+  a
 end
